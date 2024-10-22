4ef74ee8201cb90211f7df995cacd95865eb60ee
Vinicius <vdelciello@outlook.com>
2024-10-22 19:31:32 -0300

    alterações página de voluntário

--- src/voluntario/voluntario.module.ts
+++ src/voluntario/voluntario.module.ts
@@ -1,29 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-
-// import { VoluntarioController } from './voluntario.controller'; // Capitalized the controller name
-// import { VoluntarioService } from './voluntario.service';
-// import { voluntarioProviders } from './voluntario.providers';
-// import { DatabaseModule } from 'src/database/database.module';
-// import { VOLUNTARIO } from './voluntario.entity';
-// // import { validarEmail } from './validacao/email.validator'; // Uncomment if needed
-
-// @Module({
-//   imports: [
-//     DatabaseModule,
-//     TypeOrmModule.forFeature([VOLUNTARIO])  // Register VOLUNTARIO entity
-//   ],
-//   controllers: [VoluntarioController], // Capitalized the controller name
-//   providers: [
-//     ...voluntarioProviders,
-//     VoluntarioService,
-//     // validarEmail // Uncomment if needed
-//   ],
-//   exports: [VoluntarioService],
-// })
-// export class VoluntarioModule {}
-
-
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
